a5e868f3131598f294ee3d0231d35780a91422c3
Braunagel <braunagel@dhbw-ravensburg.de>
2026-04-22 14:09:22 +0200

    add solution for task1 and 2

---
+++ courses/coding-in-C++/Lab_5/solution/drone_test_bench.hpp
@@ -0,0 +1,40 @@
+#ifndef DRONE_TEST_BENCH_HPP
+#define DRONE_TEST_BENCH_HPP
+
+#include <iostream>
+
+template <typename T>
+void swap(T &firstValue, T &secondValue)
+{
+    std::cout << "Input" << '\n';
+    std::cout << "First value: " << firstValue << '\n';
+    std::cout << "Second value: " << secondValue << '\n';
+
+    T tmp = firstValue;
+    firstValue = secondValue;
+    secondValue = tmp;
+
+    std::cout << "Output" << '\n';
+    std::cout << "First value: " << firstValue << '\n';
+    std::cout << "Second value: " << secondValue << '\n';
+}
+
+// Question Task 1: Why a template is better here than writing three separate functions?
+// Answer: There are multiple reasons why templates should be chosen over three separate functions:
+// 1. Less effort for the programmer when writing the code
+// 2. Less error-prone and better to maintain because there is only one implementation
+// 3. Only instantiated when used so it can potentially reduce the binary size of the exe
+// 4. Templates are perfect for reusability since each type can be used as long as the operations are defined.
+
+template <typename T>
+void printFrame(T frame, int size)
+{
+    std::cout << "[";
+    for (int i = 0; i < size - 1; i++)
+    {
+        std::cout << frame[i] << ", ";
+    }
+    std::cout << frame[size - 1] << "]" << '\n';
+}
+
+#endif
